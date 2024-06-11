133a7eaec99211d09de69fbab53147a5febb773f
quake <quake.wang@gmail.com>
2024-06-11 18:44:51 +0900

    fix: should early return for repeat add_tlc message

--- src/ckb/channel.rs
+++ src/ckb/channel.rs
@@ -2379,13 +2379,13 @@ impl ChannelActorState {
                 if !flags.contains(CollaboratingFundingTxFlags::COLLABRATION_COMPLETED) =>
             {
                 return Err(ProcessingChannelError::InvalidState(format!(
-                    "Unable to process commitment_signed command in state {:?}, as collaboration is not completed yet.",
+                    "Unable to process commitment_signed message in state {:?}, as collaboration is not completed yet.",
                     &self.state
                 )));
             }
             ChannelState::CollaboratingFundingTx(_) => {
                 debug!(
-                    "Processing commitment_signed command in from CollaboratingFundingTx state {:?}",
+                    "Processing commitment_signed message in state {:?}",
                     &self.state
                 );
                 CommitmentSignedFlags::SigningCommitment(SigningCommitmentFlags::empty())
@@ -2400,7 +2400,7 @@ impl ChannelActorState {
             }
             ChannelState::SigningCommitment(flags) => {
                 debug!(
-                    "Processing commitment_signed command in from SigningCommitment state {:?}",
+                    "Processing commitment_signed message in state {:?}",
                     &self.state
                 );
                 CommitmentSignedFlags::SigningCommitment(flags)
@@ -2418,7 +2418,7 @@ impl ChannelActorState {
             }
             ChannelState::ChannelReady(flags) => {
                 debug!(
-                    "Processing commitment_signed command in from ChannelReady state {:?}",
+                    "Processing commitment_signed message in state {:?}",
                     &self.state
                 );
                 CommitmentSignedFlags::ChannelReady(flags)
